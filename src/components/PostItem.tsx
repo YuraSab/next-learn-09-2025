a226df48102cdd5a7fa853a8851dae0ac414ecba
YuraSab <yrasabat@gmail.com>
2025-09-22 14:20:05 +0300

    firebase-implementaion

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import { Post } from "@/types/Post";
+import Image from "next/image";
 
 interface Props {
     post: Post,
 }
 
 const PostItem = ({ post }: Props) => {
+    const imageUrl = `https://picsum.photos/400/200?random=${post.id}`;
+
     return (
         <div className="h-35 p-4 border border-gray-200 rounded-md">
+            <Image
+                src={imageUrl}
+                alt={`Image for post ${post.title}`}
+                width={400}
+                height={200}
+                className={"rounded-md mb-4"}
+            />
             <h3 className="text-xl font-semibold">{post.title}</h3>
             <p>{post.body}</p>
             <Link href={`/posts/${post.id}`} className="text-blue-500 hover:underline mt-2 inline-block">
